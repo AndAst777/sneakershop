21e8f75c14571fd001284036132a3ca061cfdb92
andrej <andrej.astakhow1@gmail.com>
2024-04-14 19:12:51 +0400

    Second commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,15 @@
+@include('components.app')
+
+<body>
+    @include('components.navbar')
+
+    <h1 class="mt-20 text-center">НОВОСТИ МОДЫ: КРОССОВКИ И ОДЕЖДА</h1>
+    <div class="grid sm:grid-cols-3 grid-cols-1 gap-4 mb-4 place-items-center items-center mt-20">
+        @foreach ($posts as $post)
+            @include('components.post_card')
+        @endforeach
+    </div>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+</body>
